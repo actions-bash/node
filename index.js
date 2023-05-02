34befdcc0f46714dd29af8a46ed9dceb981e9a83
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-05-02 09:07:38 +0800

    chore(js): add comment [skip ci]

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+// Execute the Shell script.
+// https://github.com/actions-bash/node/blob/v1.0.1/index.js
 require('child_process').spawn('bash',['./entrypoint.sh'],{stdio:'inherit'}).on('close',code=>process.exit(code));
